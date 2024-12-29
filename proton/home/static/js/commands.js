1c1112e0e2c028aeb9395ee468dcdaea4ea0fa9f
Prashith Shetty <147428351+Prashithshetty@users.noreply.github.com>
2024-12-29 16:25:52 +0530

    pushing terminal styles and other java script files

---
+++ proton/home/static/js/commands.js
@@ -0,0 +1,65 @@
+var linkedin = "https://www.linkedin.com/company/proton-nmamit/";
+var instagram = "https://www.instagram.com/proton_nmamit";
+var github = "https://github.com/prashithshetty";  
+var email = "protonemail.com"
+var members = 'path to members page homie'; 
+var GUI = 'path to GUI home page';
+about = [
+  "<br>",
+  "About Us",
+  "PROTON, the PRoactive Organisation for Threats and Online",
+  "Networks, is a distinguished cybersecurity club within the",
+  "Department of Cybersecurity. We are dedicated to advancing the",
+  "field of cybersecurity by empowering students to transform",
+  "innovative ideas into robust, real-world solutions. Our club",
+  "nurtures a culture of vigilance and continuous improvement,", 
+  "preparing the next generation of cybersecurity professionals to",
+  "tackle emerging threats with confidence.",
+  "     ",
+  "     ",
+  "At Proton, we emphasize hands-on experience and practical skills",
+  "essential for cybersecurity. We provide a platform for students to",
+  "engage in simulated cyber attacks, defense strategies, and",
+  "vulnerability assessments, ensuring they are well-equipped to handle",
+  "the complexities of the digital world. Through capture-the-flag",
+  "competitions, penetration testing exercises, and expert-led",
+  "workshops, we offer members the opportunity to sharpen their",
+  "technical skills and stay ahead in the ever-evolving cybersecurity",
+  "landscape.",
+  "<br>"
+];
+
+socialmedia = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">click here' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">click here' + '</a>',
+  'github         <a href="' + github + '" target="_blank">click here' + "</a>",
+  "<br>"
+];
+
+
+help = [
+  "<br>",
+  '<span class="command">home</span>           Display the fancy terminal art',
+  '<span class="command">about</span>          About Proton',
+  '<span class="command">members</span>        Open a page with proton memebers',
+  '<span class="command">socialmedia</span>    Displays all our social media accout links',
+  '<span class="command">help</span>           Comeon read it again',
+  '<span class="command">contact</span>        Displays offical email(dont contact us we dont actually care about you)',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">gui</span>            Takes You to Graphical interface of website(normal website);)',
+  
+  "<br>",
+];
+
+home = [
+  '<span class="index">© 2025 PROTON. All rights reserved..</span>',
+  "██████╗░██████╗░░█████╗░████████╗░█████╗░███╗░░██╗",
+  "██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗████╗░██║",
+  "██████╔╝██████╔╝██║░░██║░░░██║░░░██║░░██║██╔██╗██║",
+  "██╔═══╝░██╔══██╗██║░░██║░░░██║░░░██║░░██║██║╚████║",
+  "██║░░░░░██║░░██║╚█████╔╝░░░██║░░░╚█████╔╝██║░╚███║",
+  "╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚══╝",
+  '<span class="color2">Deal with the terminal, U dumb ass type imdumb to get our gui web interface.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
---
+++ proton/home/static/js/main.js
@@ -0,0 +1,143 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("user@proton:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+loopLines(home, "", 80);
+  
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "about":
+      loopLines(about, "color2 margin", 80);
+      break;
+  
+    case "members":
+      addLine("Opening GUI-PROTON...", "color2", 80);
+      newTab(members);
+      break;
+    case "socialmedia":
+      loopLines(socialmedia, "color2 margin", 80);
+      break;
+    case "contact":
+      addLine( 'email        <a href="' + email + '" target="_blank">email' + "</a>", "color2", 80);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "home":
+      loopLines(home, "", 80);
+      break;
+    case "gui":
+      addLine("Opening GUI-INTERFACE...", "color2", 0);
+      newTab(GUI);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
