2a102ee64441961ab5247ac32e3c32b1e1ca0f27
Rishi-Bhati <bhatirishi5@gmail.com>
2025-01-17 18:39:11 +0530

    `Added event listing page with CSS styles and updated views to display events`

--- proton/events/views.py
+++ proton/events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
 
 def events(request):
-    return render(request, 'events.html')
+    events = Event.objects.all().order_by('-created_at')
+    return render(request, 'events.html', {'events': events})
