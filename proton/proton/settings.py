96249cd18863ffee57744efe530d0f691840690b
Rishi-Bhati <bhatirishi5@gmail.com>
2025-07-12 22:19:36 +0530

    Add 'proton-eta.vercel.app' to ALLOWED_HOSTS in settings

--- proton/proton/settings.py
+++ proton/proton/settings.py
@@ -30,7 +30,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['proton-nmamit.azurewebsites.net', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['proton-nmamit.azurewebsites.net', '127.0.0.1', 'localhost', 'proton-eta.vercel.app']
 
 #, 'proton-nmamit.onrender.com', 'protonnmamit.pythonanywhere.com'
 
